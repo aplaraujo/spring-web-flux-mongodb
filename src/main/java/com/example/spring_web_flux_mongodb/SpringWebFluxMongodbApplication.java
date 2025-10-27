5e74327bc97856b83ebe7abc113813e35e39239d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-27 15:28:20 -0300

    first commit

---
+++ src/main/java/com/example/spring_web_flux_mongodb/SpringWebFluxMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_flux_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxMongodbApplication.class, args);
+	}
+
+}
